225418e2b800d6d0888e3d28f8d5019975c95fbc
Sarthak Grover <sgrover@gatech.edu>
2013-11-12 03:56:15 -0500

    remove Framer.recvFrame and Framer.flushBuffer functions
    these functions should be implemented in buffer.py. Framer is only
    responsible for assembly/disassembly while maintaining correct
    sequence numbers.

--- htpt/frame.py
+++ htpt/frame.py
@@ -78,52 +78,6 @@ class Framer():
         return False
     return False
 
-  def recvFrame(self, data, seqNum):
-    """Add the given frame to the buffer
-
-    Parameters: data- the data from the frame
-    seqNum- the sequence number of the frame
-
-    Note: the acceptable window is based on available buffer space,
-    not unacked packets
-
-    """
-    if not self.isSeqNumInBuffer(seqNum):
-      raise FramingException("Not enough space in the buffer")
-    index = (seqNum - self.minAcceptableSeqNum) % MAX_SEQ_NUM
-    self.buffer[index] = data
-    #see if we have enough data to pass up by coalescing all of the
-    #frames and seeing if we meet the min threshold to send up
-    dataToSend = 0
-    for index in range(BUFFER_SIZE):
-      if self.buffer[index] is None:
-        #we have found all sendable data
-        break
-      else:
-        dataToSend = dataToSend + len(self.buffer[index])
-    #if we have enough data to send up, pass it to the callback
-    #function
-    if dataToSend > MIN_SIZE_TO_PASS_UP:
-      availableData = ''
-      #coalesce every data element up to the first missing sequence
-      while self.buffer[0] is not None:
-        availableData += self.buffer.pop(0)
-        self.minAcceptableSeqNum = ((self.minAcceptableSeqNum +1) % BUFFER_SIZE)
-        self.maxAcceptableSeqNum = ((self.minAcceptableSeqNum +1) % BUFFER_SIZE)
-        self.buffer.append(None)
-      self.recvData(availableData)
-
-  def flushBuffer(self):
-    """Send all in order data up to the callback function"""
-
-    availableData = ''
-    while self.buffer[0] is not None:
-      availableData += self.buffer.pop(0)
-      self.minAcceptableSeqNum = ((self.minAcceptableSeqNum +1) % BUFFER_SIZE)
-      self.maxAcceptableSeqNum = ((self.minAcceptableSeqNum +1) % BUFFER_SIZE)
-      self.buffer.append(None)
-    self.recvData(availableData)
-
 
 class Assemble():
   def __init__(self):
