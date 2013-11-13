2e7224404b4004972a6d78a3f269af800fd57162
Sarthak Grover <sgrover@gatech.edu>
2013-11-12 20:49:44 -0500

    get/set SessionID(); option for SYN flag

--- htpt/frame.py
+++ htpt/frame.py
@@ -7,7 +7,6 @@ import threading
 import struct
 from random import randint
 
-import urlEncode
 from buffers import Buffer
 from constants import *
 
@@ -33,24 +32,45 @@ class SeqNumber():
 
 class Assemble():
   """Class to Assemble a data frame with headers before sending to encoder"""
-  def __init__(self):
-    """Initialize SeqNumber object"""
+  def __init__(self, sessionID=0):
+    """Initialize SeqNumber object and sessionID"""
     self.seqNum = SeqNumber()
     self.output = None
+    self.setSessionID(sessionID)
+
+  def setSessionID(self, sessionID):
+    """Function to allow upper abstraction to set sessionID for sender"""
+    self.sessionID = sessionID
+
+  def getSessionID(self):
+    """Function to retrieve the client's or server's session ID
+
+    server assigns a new session ID when a new client connects to
+    it. server maintains sessionID on its end. client should retrieve
+    sessionID from the ACK packet (from SYN-ACK) and set it as
+    self.sessionID"""
+    return self.sessionID
 
   def generateFlags(self, **kwargs):
     """Generates a 4-bit string of bits
 
     Parameters: kwargs- additional keyword arguments specified for the
-    function. Currently, the only additional option has the keyword of
-    'more_data' and a boolean value (0/1) which sets the more_data bit
-    (MSB) in the header. Example syntax: generateFlags(more_data=1)"""
+    function. Currently, the only additional option is 'more_data' and
+    'SYN', which are assigned a boolean integer value (0/1). These
+    set appropriate bits in flags.
+    Example syntax: generateFlags(more_data=1, SYN=0)
+
+    flags format: [ more_data | SYN | X | X ]"""
 
     flags = '0000'
+    flag_list = list(flags)
     if 'more_data' in kwargs:
       more_data = kwargs['more_data']
-      if more_data==1:
-        flags = '1000'
+      flag_list[0]=str(more_data)
+    if 'SYN' in kwargs:
+      SYN_flag = kwargs['SYN']
+      flag_list[1]=str(SYN_flag)
+    flags = "".join(flag_list)
     return flags
 
   def generateNonce(self):
@@ -63,11 +83,6 @@ class Assemble():
     seqNum = self.seqNum.getSequenceAndIncrement()
     return seqNum
 
-  def getSessionID(self):
-    # TODO not sure how to set session IDs
-    sessionID = 0
-    return sessionID
-
   def getHeaders(self, **kwargs):
     """Create a 4 byte struct header in network byte order
 
@@ -76,7 +91,7 @@ class Assemble():
 
     Calls functions to get:
     seqNum- 2 byte sequence number of the frame
-    sessionID - 1 byte (currently None)
+    sessionID - 1 byte char int assigned by server
     flags - 4 bit string. check kwargs and set appropriate bit
     nonce - integer. randomized value [0,15]
 
@@ -118,12 +133,14 @@ class Disassemble:
   """Class to Disassemble a decoded packet into headers+data before sending to buffers"""
   def __init__(self):
     self.output = None
+    # allocate a buffer to receive data and flush it
     self.buffer = Buffer()
     # TODO CHECK if flush() should be added here as a callback to buffer
     self.buffer.addCallback(self.flush)
 
   def disassemble(self, frame):
-    """Unassemble received (decoded)frame to headers and data
+    """
+    Disassemble received (decoded) frame to headers and data
 
     Parameters: frame is the raw bytes after decoding url or cookie
     headers are the first 4 bytes, data is what follows.
@@ -131,7 +148,8 @@ class Disassemble:
     should be called from main() after urlEncode.decode(). raw data,
     seqNum are then sent to Buffer.recvData() to flush it.
 
-    we assume data is simply a string"""
+    we assume data is simply a string
+    """
 
     # split to headers + data
     headers = frame[:4]
@@ -148,15 +166,23 @@ class Disassemble:
 
     headerTuple = struct.unpack('!HBB', headers)
     self.seqNum = headerTuple[0]
-    self.sessionID = headerTuple[1]
+    # TODO: At main, call getSessionID()
+    self.sessionID = self.setSessionID(headerTuple[1])
     mask = int('0b1111',2)
     self.flags = bin((headerTuple[2] & (mask << 4)) >> 4)[2:]
-    # TODO: if flags = '1000' i.e. more_data, then send pull_request to
+    # TODO: At main: if flags = '1000' i.e. more_data, then send pull_request to
     # server for more data
     self.nonce = headerTuple[2] & mask
 
+  def getSessionID(self):
+    """Return session ID to upper abstraction"""
+    return self.sessionID
+
+  def setSessionID(self, sessionID=0):
+    """Set sessionID at client or server"""
+    self.sessionID = sessionID
+
   def flush(self):
-    # TODO: send self.output to Tor
+    # send self.output to Tor
     print self.output
     self.output = None
-    pass
