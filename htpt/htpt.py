16488951678a767f0940670aa374df16289ae412
Ben Jones <benjonesece@gmail.com>
2013-11-18 23:57:28 -0500

    Updates to server code
    Right now, the server code cannot be finished, so I am commiting
    what I do have done for comments. There are 2 main issues
    1) the sequence num and session id code needs to be refactored.
    It really needs an initialize function and we need to make sure
    that the encode/decode functions and the framing stuff can play
    together nicely
    2) we need a way to bundle the assembler, disassembler, and Internet
    connections together. Any advice on abstractions is welcome.
    Otherwise, will probably default to a data structure which stores
    the session ID and each parameter

--- htpt/htpt.py
+++ htpt/htpt.py
@@ -24,7 +24,13 @@ TOR_BRIDGE_PASSWORD = "hello"
 # are likely different
 
 class HTPT():
-  def run_client():
+  def init(self):
+    self.addressList = []
+    self.disassembler = frame.Disassemble()
+    self.socks = []
+    self.conns = []
+
+  def run_client(self):
     # initialize the connection
     self.assembler = frame.Assemble()
     self.receivedData = False
@@ -92,20 +98,55 @@ class HTPT():
     # use the returned image to initialize the session ID
     #TODO how do we get the type of image?
     decodedData = imageEnode.decode(image, 'png')
-    self.disassembler = frame.Disassemble()
     self.disassembler.disassemble(decodedData)
     self.assembler.setSessionID(self.disassembler.getSessionID())
     return headlessWebKit
 
-  def serverRecvData(data):
+  def serverRecvData(request, ipAddress):
+    """
+    Code which basically runs the server
+
+    Parameters:
+    request- the headers for the interaction the client just made with Apache
+    ipAddress- the IP address for the host
+
+    Structure: the wsgi code is pretty dumb, so it will call this
+    function every time it receives something. This function will 1)
+    determine whether this is HTPT or image gallery traffic 2) route
+    gallery traffic to the appropriate location and 3) receive and
+    respond to the htpt traffic
+
+    """
+    # if we are not in the address list, then this is not an initialized connection
+    if ipAddress not in self.addressList:
+      # if the address is not in the list and it is not a market
+      # request, then it is web gallery traffic
+      if not isMarket(request):
+        sendToImagegallery(request)
+        return
+      # if this is a market request, then proceed with new session initialization
+      else:
+        decoded = urlEncode.decode(request)
+        self.disassembler.disassemble(decoded)
+        password = self.disassembler.flush()
+        # if they successfully authenticate, continue
+        if password == TOR_BRIDGE_PASSWORD:
+          self.addressList.append(ipAddress)
+          conn = frame.Dissassemble(self.disassember.getSeqNum())
+          #TODO
+          #set the session id to the next value
+          #setup some way to maintain a single Internet connection per client
 
   def run_server():
-    """run a server for htpt"""
+    """
+    run a server for htpt
 
-    #when Apache gets a new connection with a cookie which matches one
-    #of the authenticated cookies for this Tor bridge, the gallery code
-    #will call the create_connection function, which adds connections to
-    #our connection array, conns
+    Structure: this method basically just receives data back from the
+    Internet and forwards the traffic onto the client
+    
+    """
+    
+    #TODO-> finish implementing this code after we come up with new abstraction
     while 1:
       for conn in conns:
         # if we have received data from the Tor network for the Tor
@@ -123,39 +164,6 @@ class HTPT():
           conn.closeConnection()
           gallery.remove(conn.Address)
           conns.remove(conn)
-
-  def create_connection(url):
-    """
-    Create a connection with a client who has successfully
-    authenticated with one of the authentication cookies
-
-    Parameters:
-    url- the url from the HTTP GET request with the authentication
-    cookie. The url will not contain any data, but will have a valid
-    frame header. This header should contain a SYN flag and a sequence
-    number
-
-    Note: The server will initialize the minimum acceptable sequence
-    number to the value give in the URL's frame header
-
-    Note: HTPT will manage all state. Since the image gallery needs to
-    maintain a list of authenticated IP addresses so it knows what
-    traffic to pass through, HTPT will manage a list of IP addresses for
-    the image gallery. The image gallery exposes this list to HTPT
-    through an add and remove function
-
-    """
-    conn = conns.append(frame.initConn())
-    gallery.add(conn.Address)
-
-  def receive_data(data):
-    """Receive data from the image gallery"""
-
-    #find the connection in the list of connections
-    decoded = urlEncode.decode(data)
-    conn = findConnBySessionID(data[:4])
-    conn.timeout = datetime.now()
-    conn.receive(decoded)
 
 if __name__ == '__main__':
   run_client()
