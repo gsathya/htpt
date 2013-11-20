825d12fd77332a41c48d6e9b082faade9ac7aa21
Sarthak Grover <sgrover@gatech.edu>
2013-11-19 19:29:06 -0500

    fixed testsuite with changes to frame.py
    no Nonce functionality
    dummycallback option for receive buffers
    flags are now integers instead of strings

--- tests/verifyFrame.py
+++ tests/verifyFrame.py
@@ -83,24 +83,19 @@ class TestAssemble(unittest.TestCase):
   def test_generateFlags(self):
     # default case no flags
     self.flags = self.Assembler.generateFlags()
-    self.assertEqual(self.flags, '0000')
+    self.assertEqual(self.flags, 0)
     # only more_data flag
     self.flags = self.Assembler.generateFlags(more_data=1)
-    self.assertEqual(self.flags, '1000')
+    self.assertEqual(self.flags, 1<<7)
     # only SYN flag
     self.flags = self.Assembler.generateFlags(SYN=1)
-    self.assertEqual(self.flags, '0100')
+    self.assertEqual(self.flags, 1<<6)
     # more_data and SYN flag
     self.flags = self.Assembler.generateFlags(more_data=1, SYN=1)
-    self.assertEqual(self.flags, '1100')
+    self.assertEqual(self.flags, 3<<6)
     # some random flag should still be like default case
     self.flags = self.Assembler.generateFlags(no_fucks_to_give=1)
-    self.assertEqual(self.flags, '0000', "flags break with arbit data")
-
-  def test_generateNonce(self):
-    self.nonce = self.Assembler.generateNonce()
-    self.assertLessEqual(self.nonce, 15, "nonce out of range")
-    self.assertGreaterEqual(self.nonce, 0, "nonce out of range")
+    self.assertEqual(self.flags, 0, "flags break with arbit data")
 
   def test_getHeaders(self):
     self.Assembler.setSessionID(0)
@@ -110,37 +105,37 @@ class TestAssemble(unittest.TestCase):
     self.assertEqual(self.headerString[:2], '\x00\x01')
     # session ID should be 0
     self.assertEqual(self.headerString[2], '\x00')
-    # flags should be '0000' and nonce is random
-    self.assertEqual(self.Assembler.nonce, ord(self.headerString[3]))
+    # flags should be 0 => \x00
+    self.assertEqual(self.headerString[3], '\x00')
 
     # this should increase sequence number by one, and flags will change
     self.headerString = self.Assembler.getHeaders(more_data=1)
     self.assertEqual(self.headerString[:2], '\x00\x02')
-    self.assertEqual((int('1000', 2) << 4) | self.Assembler.nonce, ord(self.headerString[3]))
+    self.assertEqual(self.headerString[3], '\x80')
 
   def test_assemble(self):
     # less data
     self.data = '0100101010101010101010101'
-    self.Assembler.assemble(self.data, more_data=1, SYN=1)
-    self.assertEqual(self.data, self.Assembler.output[4:])
+    self.output = self.Assembler.assemble(self.data, more_data=1, SYN=1)
+    self.assertEqual(self.data, self.output[4:])
 
 
 class TestDisassemble(unittest.TestCase):
   """Test the validity of the Disassemble framing module in htpt"""
 
   def setUp(self):
-    self.callback = self.flush
+    self.callback = self.dummyCallback
     self.downloadedData = ''
-    self.Disassembler = frame.Disassemble()
+    self.Disassembler = frame.Disassemble(self.callback)
     self.dummyData = '01010101010101010101010101010100101'
-    # data, more_data set, seq num =1
-    self.test_frame1 = '\x00\x01\x00\x8e01010101010101010101010101010100101'
-    # data[:5], no flags, seq num =2
-    self.test_frame2 = '\x00\x02\x00\r01010'
-    # no data, SYN flag, seq num = 3
-    self.test_frame3 = '\x00\x03\x00A'
+    # data, more_data = 1, SYN=1, seq num =16
+    self.test_frame1 = '\x00\x10\x00\xc001010101010101010101010101010100101'
+    # data[:5], more_data = 0, SYN=1 , seq num = 19
+    self.test_frame2 = '\x00\x13\x00@01010'
+    # no data, more_data = 1, SYN=0, seq num = 21
+    self.test_frame3 = '\x00\x15\x00\x80'
 
-  def flush(self, data):
+  def dummyCallback(self, data):
     """Dummy callback function to test the framing modules ability to
     send data up"""
     print "flush data up"
@@ -154,28 +149,28 @@ class TestDisassemble(unittest.TestCase):
     self.assertEqual(len(self.Disassembler.buffer.buffer), constants.BUFFER_SIZE)
 
   def test_retrieveHeaders(self):
-    self.header = '\x00\x02\x00\r'
+    #more_data=1, SYN=0, sessionID=10 (set using setSessionID()), seqNum = 23
+    self.header = '\x00\x17\n\x80'
     self.Disassembler.retrieveHeaders(self.header)
-    self.assertEqual(self.Disassembler.seqNum, 2)
-    self.assertEqual(self.Disassembler.sessionID, 0)
-    self.assertEqual(self.Disassembler.flags, '0000')
-    self.assertEqual(self.Disassembler.nonce, 13)
+    self.assertEqual(self.Disassembler.seqNum, 23)
+    self.assertEqual(self.Disassembler.getSessionID(), 10)
+    self.assertEqual(self.Disassembler.flags, 1<<7)
 
   def test_disassemble(self):
-    self.Disassembler.disassemble(self.test_frame1)
-    self.assertEqual(self.Disassembler.seqNum, 1)
-    self.assertEqual(self.Disassembler.flags, '1000')
-    self.assertEqual(self.Disassembler.output, self.dummyData)
+    self.output = self.Disassembler.disassemble(self.test_frame1)
+    self.assertEqual(self.Disassembler.seqNum, 16)
+    self.assertEqual(self.Disassembler.flags, (1<<7 | 1<<6))
+    self.assertEqual(self.output, self.dummyData)
 
-    self.Disassembler.disassemble(self.test_frame2)
-    self.assertEqual(self.Disassembler.seqNum, 2)
-    self.assertEqual(self.Disassembler.flags, '0000')
-    self.assertEqual(self.Disassembler.output, self.dummyData[:5])
+    self.output = self.Disassembler.disassemble(self.test_frame2)
+    self.assertEqual(self.Disassembler.seqNum, 19)
+    self.assertEqual(self.Disassembler.flags, (1<<6))
+    self.assertEqual(self.output, self.dummyData[:5])
 
-    self.Disassembler.disassemble(self.test_frame3)
-    self.assertEqual(self.Disassembler.seqNum, 3)
-    self.assertEqual(self.Disassembler.flags, '0100')
-    self.assertEqual(self.Disassembler.output, '')
+    self.output = self.Disassembler.disassemble(self.test_frame3)
+    self.assertEqual(self.Disassembler.seqNum, 21)
+    self.assertEqual(self.Disassembler.flags, 1<<7)
+    self.assertEqual(self.output, '')
 
 
 
