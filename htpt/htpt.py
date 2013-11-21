4a058ebe922eea48e2abe64667440635fc9244c7
Ben Jones <benjonesece@gmail.com>
2013-11-21 08:38:30 -0500

    Added basic flask code
    Added basic function implementation using flask to get data from
    apache

--- htpt/htpt.py
+++ htpt/htpt.py
@@ -1,18 +1,26 @@
-# Ben Jonesp
+#! /usr/bin/python
+# Ben Jones
 # Fall 2013
 # Georgia Tech
 # htpt.py: this file contains the code to run the HTPT transport
 
 # imports
 from datetime import datetime
-from flask import flask, request
 import select
 import socket
-import socks
+import sys
 import urllib2
 
+#flask stuff
+from flask import Flask, request
+app = Flask(__name__)
+
 # local imports
-from htpt import frame
+import frame
+import urlEncode
+#from htpt import frame
+#from htpt import urlEncode
+#from htpt import imageEncode
 
 #Constants
 LISTEN_PORT=8000 #local port to communicate to Tor with
@@ -20,10 +28,8 @@ TIMEOUT = 0.1 #max number of seconds between calls to read from the server
 
 #Constants just to make this work-> remove
 #TODO
-TOR_BRIDGE_ADDRESS = 192.168.142.1
+TOR_BRIDGE_ADDRESS = "192.168.142.1"
 TOR_BRIDGE_PASSWORD = "hello"
-
-# TODO-change this from pseudocode to actual code
 
 # Note: I wrote this hastily, so the function names within our modules
 # are likely different
@@ -166,7 +172,7 @@ class HTPT():
     # if this is an initialized client, then receive the data and see
     # if we have anything to send
     else:
-      decoded = urlEncode.decode([request.url, request.cookies)
+      decoded = urlEncode.decode([request.url, request.cookies])
       sendToApache(self.buffer.pop())
 
   def run_server():
@@ -204,5 +210,78 @@ class HTPT():
         # send the data with apache
         self.buffer.append(encoded)
 
+@app.route('/')
+def processRequest():
+  """Process incoming requests from Apache
+  
+  Structure: this function determines whether data should go through
+  to htpt decoding or if it should be passed to the image
+  gallery. This is a function due to constraints from flask
+
+  """
+  # if we are not in the address list, then this is not an initialized connection
+  if ipAddress not in addressList:
+      # if the address is not in the list and it is not a market
+      # request, then it is web gallery traffic
+      if not urlEncode.isMarket(request):
+        sendToImageGallery(request)
+        return
+      # if this is a market request, then proceed with new session initialization
+      else:
+        decoded = urlEncode.decode(request.url)
+        password = htptObject.disassembler.initServerConnection(decoded)
+        # if they successfully authenticate, continue
+        if password == TOR_BRIDGE_PASSWORD:
+          addressList.append(ipAddress)
+          htptObject.conn = htptObject.dissassembler(htptObject.disassember.getSeqNum())
+          sessionID = htptObject.sessionIDs.getSessionIDAndIncrement()
+          htptObject.conn.setSessionID(sessionID)
+          #send back a blank image with the new session id
+          htptObject.assembler.setSessionID(sessionID)
+          image = imageEncode.encode('', 'png')
+          return serveImage(image)
+          #TODO
+          #set the session id to the next value
+          #setup some way to maintain a single Internet connection per client
+    # if this is an initialized client, then receive the data and see
+    # if we have anything to send
+  else:
+    decoded = urlEncode.decode([request.url, request.cookies])
+    readyToRead, readyToWrite, inError = \
+        select.select(htptObject.torSock, None, None, 0)
+    # if we have received data from the Tor network for the Tor
+    # client, then send it
+    if readyToRead != None:
+      # get up to a megabyte
+      dataToSend = readyToRead.recv(1024*1000)
+      # put the headers on the data (not the actual function name)
+      framed = self.assembler.assemble(dataToSend)
+      # encode the data
+      encoded = imageEncode.encode(framed, 'png')
+      # send the data with apache
+      self.buffer.append(encoded)
+      return serveImage(htptObject.buffer.pop())
+
+def serveImage(image):
+  response = make_response(image)
+  response.headers['Content-Type'] = 'image/png'
+  response.headers['Content-Disposition'] = 'attachment; filename=img.png'
+  return response
+  
 if __name__ == '__main__':
-  run_server()
+  htptObject = HTPT()
+  if str(sys.argv[1]) == "-client":
+    htptObject.run_client()
+  else:
+    addressList = []
+    # initialize the connection
+    htptObject.sessionIDs = SessionID()
+    htptObject.assembler = frame.Assemble()
+    htptObject.receivedData = False
+    # bind to a local address and wait for Tor to connect
+    htptObject.torBinder = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    htptObject.torBinder.bind(('localhost', LISTEN_PORT))
+    htptObject.torBinder.listen(1)
+    (htptObject.torSock, address) = htptObject.torBinder.accept()
+    app.run()
+    
